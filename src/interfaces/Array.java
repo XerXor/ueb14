8d0a10f005dba1c3b1b1e0e3bc2735ee88e66d3c
XerXor <XerXor88@googlemail.com>
2011-05-18 08:30:18 +0200

    hatte nicht ganz korrekt gemerged, nun ist alles wieder funktionsfähig

--- src/interfaces/Array.java
+++ src/interfaces/Array.java
@@ -1,51 +1,52 @@
 package interfaces;
 
+import exceptions.ArrayIsEmptyException;
+import exceptions.ArrayIsFullException;
+import exceptions.QueueIsEmptyException;
+import exceptions.QueueIsFullException;
+
 /**
  * Interface fuer den Array.
- * 
- * @author Alexei Felberg
- * @author Daniel Rhein
- * @version 09.05.2011
+ * @author Daniel
+ *
  */
 public interface Array {
+	
 	/**
 	 * Fuegt ein Object in das Array ein.
-	 * 
 	 * @param object
 	 * @throws IllegalArgumentException
+	 * @throws QueueIsEmptyException 
 	 */
-	public void add(Object object) throws IllegalArgumentException;
-
+	public void add(Object object) throws IllegalArgumentException,ArrayIsFullException, QueueIsEmptyException;
 	/**
 	 * Entfernt ein Objekt aus dem Array.
-	 * 
 	 * @param object
 	 * @throws IllegalArgumentException
+	 * @throws QueueIsFullException 
 	 */
-	public void remove(Object object) throws IllegalArgumentException;
-
+	public void remove(Object object) throws IllegalArgumentException,ArrayIsEmptyException, QueueIsFullException;
 	/**
 	 * Liefert ein Element aus dem Array
-	 * 
 	 * @param index
 	 * @throws IllegalArgumentException
 	 */
 	public Object getElement(int index) throws IllegalArgumentException;
-
 	/**
 	 * Liefert die groese des Arrays zurueck.
-	 * 
 	 * @return
 	 */
 	public int getSize();
-
 	/**
 	 * Gibt an, ob das Array voll oder nicht voll ist.
-	 * 
 	 * @return true, wenn der Array leer ist.
 	 */
 	public boolean isFull();
-
+	/**
+	 * Gibt an, ob das Array leer ist oder nicht.
+	 * @return true, wenn der Array leer ist.
+	 */
+	public boolean isEmpty();
 	/**
 	 * Gibt an, ob ein Object in dem Array vorhanden ist, oder nicht.
 	 * 
